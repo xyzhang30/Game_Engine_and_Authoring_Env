533ed9ebcb4bcd24a1efbb88b54ccb7e4192f733
Alisha Zhang <xz352@duke.edu>
2024-04-02 23:59:19 -0400

    ISSUE-9: added win condition

--- src/main/java/oogasalad/model/gameengine/RulesRecord.java
+++ src/main/java/oogasalad/model/gameengine/RulesRecord.java
@@ -4,4 +4,4 @@ import java.util.Map;
 import oogasalad.Pair;
 import oogasalad.model.gameengine.command.Command;
 
-public record RulesRecord(int maxRounds, int maxTurns, Map<Pair, java.util.List<Command>> collisionHandlers) {}
+public record RulesRecord(int maxRounds, int maxTurns, Map<Pair, java.util.List<Command>> collisionHandlers, Command winCondition) {}
