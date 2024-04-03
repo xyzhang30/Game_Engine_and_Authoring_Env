1b5ade74ba8a590ac49f6692881a1da3fd4e421d
Judy He <yh381@duke.edu>
2024-04-02 20:46:41 -0400

    [ISSUE 15] Added friction field to CollidableObject

--- src/main/java/oogasalad/model/gameparser/CollidableObject.java
+++ src/main/java/oogasalad/model/gameparser/CollidableObject.java
@@ -3,10 +3,11 @@ package oogasalad.model.gameparser;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
-record CollidableObject(@JsonProperty("collidable_id") String collidableId,
+record CollidableObject(@JsonProperty("collidable_id") int collidableId,
                         List<String> properties,
                         double mass,
                         Position position,
                         String shape,
                         Dimension dimension,
-                        List<Integer> color) {}
+                        List<Integer> color,
+                        double friction) {}
