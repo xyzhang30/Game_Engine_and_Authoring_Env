1edb6be70a9a74a7f5c30423c0f5f9644d485b67
Kevin Deng <kd254@duke.edu>
2024-04-03 01:33:06 -0400

    Updated javadoc for update() method in VisualElement interface

--- src/main/java/oogasalad/view/VisualElements/VisualElement.java
+++ src/main/java/oogasalad/view/VisualElements/VisualElement.java
@@ -7,7 +7,12 @@ import oogasalad.model.api.CollidableRecord;
  * Represents a Node in a scene on view side.
  */
 interface VisualElement {
-  void update(CollidableRecord record); // Update based on a collidable record
+
+  /**
+   * Updates the Element properties.
+   * @param record  Record for this Element's corresponding model object.
+   */
+  void update(CollidableRecord record);
 //  void render(); // Renders the visual element
 //  void updatePosition(double x, double y); // Updates the element's position
 //  void setVisible(boolean visible); // Sets the element's visibility
