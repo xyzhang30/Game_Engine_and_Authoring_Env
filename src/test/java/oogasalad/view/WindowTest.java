4e91ac335a65bb6c7063bfc0006e580c6caab1f3
Kevin Deng <kd254@duke.edu>
2024-04-03 00:16:23 -0400

    Added test file

---
+++ src/test/java/oogasalad/view/WindowTest.java
@@ -0,0 +1,18 @@
+package oogasalad.view;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WindowTest {
+  @BeforeEach
+  void setup(){
+
+  }
+
+  @Test
+  void TestSplashScreen(){
+
+  }
+}
