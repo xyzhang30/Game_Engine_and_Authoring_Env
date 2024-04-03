6050348cd5f1ed167cbb049fc35992910771ee32
Kevin Deng <kd254@duke.edu>
2024-04-03 01:31:25 -0400

    Game Element class designed tentatively

--- src/main/java/oogasalad/view/VisualElements/GameElement.java
+++ src/main/java/oogasalad/view/VisualElements/GameElement.java
@@ -1,14 +1,47 @@
 package oogasalad.view.VisualElements;
 
 import javafx.scene.Node;
+import oogasalad.model.api.CollidableRecord;
+
+// Temporary imports to be expunged after data dependency established *see InitializeDefaultNode()
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
- * Class for a single View Element for the Game engine
+ * Class for a single View Element for the Game engine.
  */
 public class GameElement implements VisualElement{
-  private Node myNode;
-  public GameElement(){
-
+  private final Node myNode;
+  private final int id;
+  public GameElement(CollidableRecord model){
+    id = model.id();
+    myNode = InitializeDefaultNode(id);
+    update(model);
   }
 
+  /**
+   * Initializes a new node's sprite based on data from the CSS file.
+   * @param id  ID number of Node to initialize.
+   */
+  private Node InitializeDefaultNode(int id){
+    return new Circle(10, Color.RED); // TO BE IMPLEMENTED! TALK TO ALISHA/JUDY
+  }
+
+  /**
+   * Updates the Element properties.
+   * @param model  This Element's corresponding model object.
+   */
+  public void update(CollidableRecord model){
+    myNode.setTranslateX(model.x());
+    myNode.setTranslateY(model.y());
+    myNode.setVisible(model.visible());
+  }
+
+  /**
+   * Returns the Node housed by this Element.
+   * @return Node  This Element's Node.
+   */
+  public Node getNode(){
+    return myNode;
+  }
 }
--- src/main/java/oogasalad/view/VisualElements/VisualElement.java
+++ src/main/java/oogasalad/view/VisualElements/VisualElement.java
@@ -2,8 +2,12 @@ package oogasalad.view.VisualElements;
 
 import oogasalad.model.api.CollidableRecord;
 
+/**
+ * Interface for a Visual Element.
+ * Represents a Node in a scene on view side.
+ */
 interface VisualElement {
-//  void update(CollidableRecord record); // Update based on a collidable record
+  void update(CollidableRecord record); // Update based on a collidable record
 //  void render(); // Renders the visual element
 //  void updatePosition(double x, double y); // Updates the element's position
 //  void setVisible(boolean visible); // Sets the element's visibility
