40084e192e9cb550616434411f9f252c208c3b57
Judy He <yh381@duke.edu>
2024-04-02 21:38:07 -0400

    [ISSUE #17] implemented parsing collidables

--- src/main/java/oogasalad/model/gameparser/GameLoaderModel.java
+++ src/main/java/oogasalad/model/gameparser/GameLoaderModel.java
@@ -1,7 +1,13 @@
 package oogasalad.model.gameparser;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import oogasalad.model.gameengine.PlayerContainer;
 import oogasalad.model.gameengine.RulesRecord;
+import oogasalad.model.gameengine.collidable.Collidable;
 import oogasalad.model.gameengine.collidable.CollidableContainer;
+import oogasalad.model.gameengine.collidable.Moveable;
+import oogasalad.model.gameengine.collidable.Surface;
 
 public class GameLoaderModel extends GameLoader {
 
@@ -10,10 +16,10 @@ public class GameLoaderModel extends GameLoader {
   private RulesRecord rulesRecord;
 
   public GameLoaderModel(int id) {
-
-    createPlayerContainer();
-    createCollidableContainer();
-    createRulesRecord();
+    super(id);
+    this.createPlayerContainer();
+    this.createCollidableContainer();
+    this.createRulesRecord();
   }
 
   // alisha
@@ -28,12 +34,44 @@ public class GameLoaderModel extends GameLoader {
 
   // judy
   private void createCollidableContainer() {
+    List<CollidableObject> collidableObjects = gameData.collidableObjects();
+    Map<Integer, Collidable> collidables = new HashMap<>();
 
-
+    for (CollidableObject co: collidableObjects) {
+      Collidable collidable = null;
+      if (co.properties().contains("movable")) {
+        collidable = createMovableCollidable(co);
+      }
+      else if (co.properties().contains("surface")) {
+        collidable = createSurfaceCollidable(co);
+      }
+      collidables.put(co.collidableId(), collidable);
+    }
+    this.collidableContainer = new CollidableContainer(collidables);
 
 
   }
 
+  private Collidable createMovableCollidable(CollidableObject co) {
+    return new Moveable(
+        co.collidableId(),
+        co.mass(),
+        co.position().xPosition(),
+        co.position().yPosition(),
+        co.properties().contains("visible")
+    );
+  }
+
+  private Collidable createSurfaceCollidable(CollidableObject co) {
+    return new Surface(
+        co.collidableId(),
+        co.mass(),
+        co.position().xPosition(),
+        co.position().yPosition(),
+        co.properties().contains("visible"),
+        co.friction()
+    );
+  }
 
   public CollidableContainer getCollidableContainer(){
     return collidableContainer;
