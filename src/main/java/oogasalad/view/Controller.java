f91e03db69ee2ba3e4433a3d5d3231a277f225fb
Noah Loewy <noah.loewy@duke.edu>
2024-04-04 04:27:54 -0400

    Ball Bouncing

--- src/main/java/oogasalad/view/Controller.java
+++ src/main/java/oogasalad/view/Controller.java
@@ -86,7 +86,8 @@ public class Controller {
    * @param fractionalVelocity velocity as fraction of maxVelocity
    */
   public void hitPointScoringObject(double fractionalVelocity, double angle){
-    gameEngine.applyInitialVelocity(100*fractionalVelocity, angle, 8); // The 8 has been hard coded!
+    gameEngine.applyInitialVelocity(1000*fractionalVelocity, angle, 8); // The 8 has been hard
+    // coded!
     animationManager.runAnimation(this);
   }
 
