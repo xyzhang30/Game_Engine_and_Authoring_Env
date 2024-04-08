f6fae492d568148031c3f5dd88110debffa793bb
Doga Ozmen <odo4@duke.edu>
2024-04-07 20:02:50 -0400

    added color choosing for authoring environment

--- src/main/java/oogasalad/view/AuthoringScreens/SurfaceSelectionScreen.java
+++ src/main/java/oogasalad/view/AuthoringScreens/SurfaceSelectionScreen.java
@@ -4,14 +4,16 @@ import javafx.scene.control.Button;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.control.ColorPicker;
 
 public class SurfaceSelectionScreen extends AuthoringScreen{
 
-  private final int backgroundWidth = 500;
-  private final int backgroundHeight = 500;
+  private final int backgroundWidth = 1000;
+  private final int backgroundHeight = 1000;
 
   public SurfaceSelectionScreen(){
     createScene();
@@ -39,11 +41,12 @@ public class SurfaceSelectionScreen extends AuthoringScreen{
   }
 
   private void createBackgroundOptionBox(){
-    Button colorButton = new Button("Color");
-    colorButton.setPrefSize(200, 100);
-    StackPane.setAlignment(colorButton, Pos.TOP_RIGHT);
-    StackPane.setMargin(colorButton, new Insets(50, 50, 0, 0));
-    root.getChildren().add(colorButton);
+    ColorPicker colorPicker = new ColorPicker();
+    colorPicker.setPrefSize(200, 100);
+    StackPane.setAlignment(colorPicker, Pos.TOP_RIGHT);
+    StackPane.setMargin(colorPicker, new Insets(50, 50, 0, 0));
+    root.getChildren().addAll(colorPicker);
+
 
     Button imageButton = new Button("Image");
     imageButton.setPrefSize(200, 100);
@@ -51,4 +54,6 @@ public class SurfaceSelectionScreen extends AuthoringScreen{
     StackPane.setMargin(imageButton, new Insets(160, 50, 0, 0));
     root.getChildren().add(imageButton);
   }
+
+
 }
