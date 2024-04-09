095536542d6cd601e97939f6449c897e42968611
Alisha Zhang <xz352@duke.edu>
2024-04-08 20:22:28 -0400

    #36 added rules builder

--- src/main/java/oogasalad/model/gamebuilder/RulesBuilder.java
+++ src/main/java/oogasalad/model/gamebuilder/RulesBuilder.java
@@ -2,11 +2,12 @@ package oogasalad.model.gamebuilder;
 
 import java.util.List;
 import oogasalad.model.api.data.GameData;
+import oogasalad.model.api.data.Rules;
 
 public class RulesBuilder implements GameBuilder{
 
   @Override
   public void buildGameField(GameData gameData, List<Record> gameField) {
-
+    gameData.setRules((Rules)gameField.get(0));
   }
 }
