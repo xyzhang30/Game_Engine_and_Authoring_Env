294fe74ecde1e8da2541307f949f7152d531626b
Alisha Zhang <xz352@duke.edu>
2024-04-10 23:52:06 -0400

    view loader test

--- src/test/java/oogasalad/model/GameLoaderViewTest.java
+++ src/test/java/oogasalad/model/GameLoaderViewTest.java
@@ -29,16 +29,14 @@ public class GameLoaderViewTest {
     }
   }
 
-//  @Test
-//  public void wrongRgbValueTest(){
-//    GameLoaderView loaderView = new GameLoaderView("testingBadRgbValue");
-//    List<ViewCollidableRecord> collidableRecords = loaderView.getViewCollidableInfo();
-//    for (ViewCollidableRecord record : collidableRecords){
-//      if (record.id() == 2 || record.id() == 3){
-//        assertEquals("circle", record.shape());
-//      } else {
-//        assertEquals("rectangle", record.shape());
-//      }
-//    }
-//  }
+  @Test
+  public void wrongRgbValueTest(){
+    GameLoaderView loaderView = new GameLoaderView("testBadRgbValue");
+    List<ViewCollidableRecord> collidableRecords = loaderView.getViewCollidableInfo();
+    for (ViewCollidableRecord record : collidableRecords) {
+      if (record.id() == 5) {
+        assertEquals(0, record.color().get(0));
+      }
+    }
+  }
 }
