86c9425189f92a3ec8fbd3086e2340f18cc66c1e
Kevin Deng <kd254@duke.edu>
2024-04-10 23:19:30 -0400

    Cleaned up GameContentPanel to have more streamlined functionality

--- src/main/java/oogasalad/view/GameScreens/GameContentPanel.java
+++ src/main/java/oogasalad/view/GameScreens/GameContentPanel.java
@@ -2,31 +2,35 @@ package oogasalad.view.GameScreens;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import oogasalad.view.VisualElements.CompositeElement;
 
 public class GameContentPanel {
   private final Pane globalView;
+  private final Rectangle globalSheen;
   private double scaleX;
   private double scaleY;
   private double offsetX;
   private double offsetY;
   public GameContentPanel(CompositeElement compositeElement){
     globalView = new AnchorPane();
-    addNodes(compositeElement);
-    globalView.getChildren().add(new Rectangle(globalView.getBoundsInLocal().getWidth(),globalView.getBoundsInLocal().getHeight(),Color.TRANSPARENT));
+    globalSheen = new Rectangle();
+    addGameContentNodes(compositeElement);
     scaleX = 1;
     scaleY = 1;
     offsetX = 0;
     offsetY = 0;
   }
-  public void addNodes(CompositeElement cm){
+  public void addGameContentNodes(CompositeElement cm){
     for (int i : cm.idList()) {
       globalView.getChildren().add(cm.getNode(i));
     }
+    globalSheen.setWidth(globalView.getBoundsInLocal().getWidth());
+    globalSheen.setHeight(globalView.getBoundsInLocal().getHeight());
+    globalSheen.setOpacity(0);
+    globalView.getChildren().add(globalSheen);
   }
-  public void SyncLocalView(){
+  public void TransformLocal(){
     globalView.setScaleX(scaleX);
     globalView.setScaleY(scaleY);
     globalView.setTranslateX(globalView.getBoundsInLocal().getCenterX()*(scaleX-1)-offsetX);
@@ -35,10 +39,9 @@ public class GameContentPanel {
   public void modifyScope(double del){
     scaleX *= del;
     scaleY *= del;
-    SyncLocalView();
+    TransformLocal();
   }
   public Pane getPane(){
-    SyncLocalView();
     return globalView;
   }
 }
