72fb3f651f665e12b81c017bad3a70146c7aec12
Judy He <yh381@duke.edu>
2024-04-16 02:29:25 -0400

    ISSUE #79 AuthoringScreen version 1

--- src/main/java/oogasalad/view/authoring_environment/AuthoringScreen.java
+++ src/main/java/oogasalad/view/authoring_environment/AuthoringScreen.java
@@ -1,10 +1,13 @@
 package oogasalad.view.authoring_environment;
 
+import java.util.List;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
@@ -12,46 +15,107 @@ import javafx.scene.text.Text;
 import oogasalad.view.Window;
 import oogasalad.view.authoring_environment.authoring_screens.NonControllableType;
 import oogasalad.view.authoring_environment.panels.AuthoringProxy;
+import oogasalad.view.authoring_environment.panels.ColorPanel;
+import oogasalad.view.authoring_environment.panels.ImagePanel;
+import oogasalad.view.authoring_environment.panels.InteractionPanel;
+import oogasalad.view.authoring_environment.panels.NonControllableShapePanel;
+import oogasalad.view.authoring_environment.panels.ShapePanel;
 import oogasalad.view.authoring_environment.panels.ShapeProxy;
 
 public class AuthoringScreen {
 
-  // initialize canvas, stagnant elements
-  // initialize different panel depending on current screen
-  // different endSelection strategy depending on current screen
-
-  // FIND OUT HOW IT WORKS:
-  // endSelection() -> controller.startNextSelection(ImageType.BACKGROUND, canvas, posMap, nonControllableMap, new ArrayList<>(), imageMap);
-  //
-
   private final AnchorPane rootPane = new AnchorPane();
-  private final StackPane canvas = new StackPane();
+//  private final StackPane rootPane = new StackPane(); // PASSED TO PANELS
+  private final VBox containerVBox = new VBox(); // PASSED TO PANELS
+  private final StackPane canvasPane = new StackPane();
   private final ShapeProxy shapeProxy = new ShapeProxy();
   private final AuthoringProxy authoringProxy = new AuthoringProxy();
+  ComboBox<String> screensDropDown = new ComboBox<>();
+  Text titleText = new Text();
   private final Scene scene;
+  private final Container container = new Container();
 
   public AuthoringScreen() {
+    createCanvas();
+//    createShapesPane();
+    createContainerVBox();
+    createScreenSelectionDropDown(List.of("Background", "Controllable", "Non-Controllable", "Interaction"));
+    handleScreenSelectionDropDown();
+    createFinishButton();
+    containerVBox.getChildren().add(titleText);
     scene = new Scene(rootPane, Window.SCREEN_WIDTH, Window.SCREEN_HEIGHT);
+    setScene("Background");
+  }
+  private void resetScene() {
+    containerVBox.getChildren().clear();
+    containerVBox.getChildren().add(titleText);
   }
 
-  private void createTitle(String title) {
-    Text titleText = new Text(title);
+  private void setScene(String screenTitle) {
+    setTitle(screenTitle);
+    // TODO: BAD DESIGN -> WHERE and HOW to set the Container?
+    switch (screenTitle) {
+      case "Background" ->
+          container.setPanels(List.of(new ColorPanel(shapeProxy, containerVBox), new ImagePanel(authoringProxy, shapeProxy,
+              containerVBox)));
+      case "Controllable" ->
+          container.setPanels(List.of(new ColorPanel(shapeProxy, containerVBox), new ImagePanel(authoringProxy, shapeProxy,
+              containerVBox), new ShapePanel(authoringProxy, shapeProxy, rootPane,
+              containerVBox, canvasPane)));
+      case "Non-Controllable" ->
+          container.setPanels(List.of(new ColorPanel(shapeProxy, containerVBox), new ImagePanel(authoringProxy, shapeProxy,
+              containerVBox), new NonControllableShapePanel(authoringProxy, shapeProxy, rootPane,
+              containerVBox, canvasPane)));
+      case "Interaction" ->
+          container.setPanels(List.of(new InteractionPanel(authoringProxy, shapeProxy, rootPane,
+              containerVBox, canvasPane)));
+    }
+
+  }
+  public Scene getScene() {
+    return this.scene;
+  }
+  private void setTitle(String title) {
+    titleText.setText(title);
     titleText.setId("titleText");
     // TODO: REMOVE HARD-CODING
     titleText.setFont(Font.font("Arial", 30));
     AnchorPane.setTopAnchor(titleText, 5.0);
     AnchorPane.setLeftAnchor(titleText, 50.0);
-    rootPane.getChildren().add(titleText);
   }
 
-  private void createAuthoringBox() {
+//  private void createShapesPane() {
+//    // TODO: REMOVE HARD-CODING
+//    int width = 980;
+//    int height = 980;
+//    rootPane.setMaxSize(width, height);
+//    rootPane.setId("rootPane");
+//    AnchorPane.setTopAnchor(rootPane, 50.0);
+//    AnchorPane.setLeftAnchor(rootPane, 50.0);
+//    rootPane.getChildren().add(rootPane);
+//  }
+
+  private void createContainerVBox() {
     // TODO: REMOVE HARD-CODING
-    int canvasWidth = 980;
-    int canvasHeight = 980;
-    canvas.setMaxSize(canvasWidth, canvasHeight);
-    canvas.setId("canvas");
-    AnchorPane.setTopAnchor(canvas, 50.0);
-    AnchorPane.setLeftAnchor(canvas, 50.0);
+    int width = 500;
+    int height = 980;
+    containerVBox.setMaxSize(width, height);
+    containerVBox.setId("rootPane");
+    containerVBox.setSpacing(20.0);
+    AnchorPane.setTopAnchor(containerVBox, 30.0);
+    AnchorPane.setRightAnchor(containerVBox, 30.0);
+    rootPane.getChildren().add(containerVBox);
+  }
+
+  private void createCanvas() {
+    // TODO: REMOVE HARD-CODING
+    int canvasWidth = 900;
+    int canvasHeight = 900;
+    canvasPane.setMaxSize(canvasWidth, canvasHeight);
+    canvasPane.setId("canvasPane");
+    AnchorPane.setTopAnchor(canvasPane, 50.0);
+    AnchorPane.setLeftAnchor(canvasPane, 70.0);
+
 
     Rectangle background = new Rectangle(canvasWidth, canvasHeight);
     background.setId("background");
@@ -63,21 +127,42 @@ public class AuthoringScreen {
     shapeProxy.setShape(background);
     authoringProxy.addNonControllableShape(background, NonControllableType.SURFACE);
 //    authoringProxy.getAuthoringController().setBackground(background);
-
-    canvas.getChildren().add(background);
-    rootPane.getChildren().add(canvas);
+    rootPane.getChildren().add(canvasPane);
+    canvasPane.getChildren().add(background);
   }
 
   private void createFinishButton() {
     // TODO: REMOVE HARD-CODING
-    Button nextButton = new Button("Finish");
-    nextButton.setId("finishButton");
-    nextButton.setPrefSize(100, 50);
-    nextButton.setOnMouseClicked(event -> authoringProxy.completeAuthoring());
-    AnchorPane.setBottomAnchor(nextButton, 50.0);
-    AnchorPane.setRightAnchor(nextButton, 50.0);
-    rootPane.getChildren().add(nextButton);
+    Button finishButton = new Button("Finish");
+    finishButton.setId("finishButton");
+    finishButton.setPrefSize(100, 50);
+    finishButton.setOnMouseClicked(event -> authoringProxy.completeAuthoring());
+    AnchorPane.setBottomAnchor(finishButton, 50.0);
+    AnchorPane.setRightAnchor(finishButton, 50.0);
+    rootPane.getChildren().add(finishButton);
   }
 
+  private void createScreenSelectionDropDown(List<String> screenOptions) {
+    screensDropDown.getItems().addAll(screenOptions);
+    screensDropDown.setPromptText("Select Screen Type");
+    AnchorPane.setTopAnchor(screensDropDown, 10.0);
+    AnchorPane.setLeftAnchor(screensDropDown, 10.0);
+    screensDropDown.setPrefSize(300, 30);
+    rootPane.getChildren().add(screensDropDown);
+  }
+
+  private void handleScreenSelectionDropDown() {
+    screensDropDown.valueProperty().addListener((obs, oldVal, selectedScreen) -> {
+      if (selectedScreen != null) {
+        resetScene();
+        setScene(selectedScreen);
+        authoringProxy.setCurrentScreenTitle(selectedScreen);
+        authoringProxy.updateScreen();
+      }
+    });
+  }
+  public AuthoringProxy getAuthoringProxy() {
+    return authoringProxy;
+  }
 
 }
