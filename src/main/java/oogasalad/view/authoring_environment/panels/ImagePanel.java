274342b36710d33878d0e9e75dd906d00f987028
Judy He <yh381@duke.edu>
2024-04-16 01:31:35 -0400

    ISSUE #73 Refactored ColorPicker

--- src/main/java/oogasalad/view/authoring_environment/panels/ImagePanel.java
+++ src/main/java/oogasalad/view/authoring_environment/panels/ImagePanel.java
@@ -3,29 +3,24 @@ package oogasalad.view.authoring_environment.panels;
 import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Shape;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import oogasalad.view.authoring_environment.authoring_screens.ImageType;
 
 public class ImagePanel implements Panel {
   private final ShapeProxy shapeProxy;
-  private final AnchorPane rootPane;
+  private final AuthoringProxy authoringProxy;
+  private final VBox containerVBox;
   private Button imageButton;
-  private final Map<Shape, String> imageMap = new HashMap<>();
-  private List<Consumer<Shape>> observers;
-  public ImagePanel(ShapeProxy shapeProxy, AnchorPane rootPane) {
-    this.rootPane = rootPane;
+  public ImagePanel(AuthoringProxy authoringProxy, ShapeProxy shapeProxy, VBox containerVBox) {
+    this.containerVBox = containerVBox;
+    this.authoringProxy = authoringProxy;
     this.shapeProxy = shapeProxy;
     createElements();
     handleEvents();
@@ -38,16 +33,16 @@ public class ImagePanel implements Panel {
     imageButton.setPrefSize(200, 100);
     AnchorPane.setTopAnchor(imageButton, 160.0);
     AnchorPane.setRightAnchor(imageButton, 50.0);
-    rootPane.getChildren().add(imageButton);
+    containerVBox.getChildren().add(imageButton);
 
   }
 
   @Override
   public void handleEvents() {
     imageButton.setOnAction(event -> {
-      String relativePath = chooseImage((Text) rootPane.lookup("#titleText"));
+      String relativePath = chooseImage((Text) containerVBox.lookup("#titleText"));
       if (relativePath != null && shapeProxy.getShape() != null) {
-        imageMap.put(shapeProxy.getShape(), relativePath);
+        authoringProxy.addImage(shapeProxy.getShape(), relativePath);
         System.out.println(relativePath);
         String imgPath = Paths.get(relativePath).toUri().toString();
         shapeProxy.getShape().setFill(new ImagePattern(new Image(imgPath)));
