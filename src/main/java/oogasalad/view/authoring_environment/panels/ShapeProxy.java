c45b3e7b04a36b7cc0890939d819fa518d060e20
Judy He <yh381@duke.edu>
2024-04-16 16:03:45 -0400

    ISSUE #78 Upated ShapeProxy, AuthoringProxy

--- src/main/java/oogasalad/view/authoring_environment/panels/ShapeProxy.java
+++ src/main/java/oogasalad/view/authoring_environment/panels/ShapeProxy.java
@@ -1,5 +1,10 @@
 package oogasalad.view.authoring_environment.panels;
 
+import java.util.List;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class ShapeProxy {
@@ -9,6 +14,20 @@ public class ShapeProxy {
   }
   public void setShape(Shape shape) {
     this.shape = shape;
+  }
+  public List<Shape> createTemplateShapes() {
+    Rectangle rectangle = new Rectangle(100, 50, Color.BLACK);
+    AnchorPane.setRightAnchor(rectangle, 150.0);
+    AnchorPane.setTopAnchor(rectangle, 300.0);
+    rectangle.setId("rectangleTemplate");
+
+    Ellipse ellipse = new Ellipse(30, 30);
+    ellipse.setFill(Color.BLACK);
+    AnchorPane.setRightAnchor(ellipse, 50.0);
+    AnchorPane.setTopAnchor(ellipse, 300.0);
+    ellipse.setId("ellipseTemplate");
+
+    return List.of(rectangle, ellipse);
   }
 
 }
