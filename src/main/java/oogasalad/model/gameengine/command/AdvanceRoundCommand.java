62b93b03c949d878f840ce5a4136fd41a23f1930
Noah Loewy <noah.loewy@duke.edu>
2024-04-17 09:28:32 -0400

    Advance Round/Turn Command Documentation

--- src/main/java/oogasalad/model/gameengine/command/AdvanceRoundCommand.java
+++ src/main/java/oogasalad/model/gameengine/command/AdvanceRoundCommand.java
@@ -4,13 +4,33 @@ import java.util.List;
 import oogasalad.model.annotations.ExpectedParamNumber;
 import oogasalad.model.gameengine.GameEngine;
 
+/**
+ * The AdvanceRoundCommand handles the internal transition from one round to another in the ooga
+ * salad game engine
+ *
+ * @author Noah Loewy
+ */
+
 public class AdvanceRoundCommand implements Command {
 
+  /**
+   * Constructs an instance of AdvanceRoundCommand with a list of arguments. This constructor does
+   * not actually do anything, and exists for the sake of consistency across commands.
+   *
+   * @param arguments, an empty list
+   */
 
   @ExpectedParamNumber(0)
   public AdvanceRoundCommand(List<Double> arguments) {
 
   }
+
+  /**
+   * Delegates the work to the engine advanceRound function, which handles the logic for actually
+   * updating the current round in the game.
+   *
+   * @param engine the GameEngine instance
+   */
 
   @Override
   public void execute(GameEngine engine) {
--- src/main/java/oogasalad/model/gameengine/command/AdvanceTurnCommand.java
+++ src/main/java/oogasalad/model/gameengine/command/AdvanceTurnCommand.java
@@ -4,14 +4,35 @@ import java.util.List;
 import oogasalad.model.annotations.ExpectedParamNumber;
 import oogasalad.model.gameengine.GameEngine;
 
+/**
+ * The AdvanceYutnCommand handles the internal transition from one turn to another in the ooga
+ * salad game engine
+ *
+ * @author Noah Loewy
+ */
+
 public class AdvanceTurnCommand implements Command {
 
   private final List<Double> arguments;
+
+  /**
+   * Constructs an instance of AdvanceTurnCommand with a list of arguments. This constructor does
+   * not actually do anything, and exists for the sake of consistency across commands.
+   *
+   * @param arguments, an empty list
+   */
 
   @ExpectedParamNumber(0)
   public AdvanceTurnCommand(List<Double> arguments) {
     this.arguments = arguments;
   }
+
+  /**
+   * Delegates the work to the engine advanceTurn function, which handles the logic for actually
+   * updating the current turn in the game.
+   *
+   * @param engine the GameEngine instance
+   */
 
   @Override
   public void execute(GameEngine engine) {
