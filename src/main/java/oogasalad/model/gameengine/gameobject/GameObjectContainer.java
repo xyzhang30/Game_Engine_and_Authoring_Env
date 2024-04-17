4daeeed65c62ef333c66e0562458eb31f3108fc9
Noah Loewy <noah.loewy@duke.edu>
2024-04-17 11:10:25 -0400

    Game Object Package Documentation Done

--- src/main/java/oogasalad/model/gameengine/gameobject/GameObjectContainer.java
+++ src/main/java/oogasalad/model/gameengine/gameobject/GameObjectContainer.java
@@ -8,12 +8,32 @@ import java.util.Set;
 import java.util.Stack;
 import oogasalad.Pair;
 import oogasalad.model.api.GameObjectRecord;
+import oogasalad.model.gameengine.player.Player;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The GameObjectContainer class manages a collection of GameObjects within the game environment,
+ * and provides efficient access to GameObjects to be accessed/manipulated via their unique IDs,
+ * while also encapsulating the map from object IDs to GameObject instances, encapsulating the
+ * implementation of the Collidables.
+ *
+ * @author Noah Loewy
+ */
+
 
 public class GameObjectContainer {
 
+  private static final Logger LOGGER = LogManager.getLogger(GameObjectContainer.class);
   private final Map<Integer, GameObject> myGameObjects;
   private final Stack<List<GameObjectRecord>> gameObjectHistory;
   private final CollisionDetector collisionDetector;
+
+  /**
+   * Constructs a GameObjectContainer with the specified collection of GameObjects.
+   *
+   * @param gameObjects A map containing GameObject instances indexed by their unique IDs.
+   */
 
   public GameObjectContainer(Map<Integer, GameObject> gameObjects) {
     myGameObjects = gameObjects;
@@ -22,9 +42,25 @@ public class GameObjectContainer {
     collisionDetector = new CollisionDetector();
   }
 
+  /**
+   * Retrieves the GameObject associated with the specified ID.
+   *
+   * @param objectId The unique ID of the GameObject to retrieve.
+   * @return The GameObject corresponding to the given ID
+   */
+
   public GameObject getGameObject(int objectId) {
+    if(!myGameObjects.containsKey(objectId)) {
+      LOGGER.warn("Game Object " + objectId + " not found");
+    }
     return myGameObjects.get(objectId);
   }
+
+  /**
+   * Checks if all visible GameObjects in the container are static (not moving).
+   *
+   * @return True if all visible GameObjects are static, otherwise false.
+   */
 
   public boolean checkStatic() {
     for (GameObject go : myGameObjects.values()) {
@@ -35,12 +71,25 @@ public class GameObjectContainer {
     return true;
   }
 
+  /**
+   * Simultaneously updates the positions of all GameObjects in the container based on their
+   * velocities and the specified time step.
+   *
+   * @param dt The time step for the update.
+   */
+
   public void update(double dt) {
     for (GameObject go : myGameObjects.values()) {
       go.move(dt);
       go.update();
     }
   }
+
+  /**
+   * Converts all GameObjects in the container to a list of immutable GameObjectRecords.
+   *
+   * @return A list of GameObjectRecords representing the current state of all GameObjects.
+   */
 
   public List<GameObjectRecord> toGameObjectRecords() {
     List<GameObjectRecord> ret = new ArrayList<>();
@@ -50,6 +99,13 @@ public class GameObjectContainer {
     return ret;
   }
 
+  /**
+   * Retrieves the immutable GameObjectRecord associated with the specified object ID.
+   *
+   * @param id The unique identifier of the GameObjectRecord to retrieve.
+   * @return The GameObjectRecord corresponding to the given ID.
+   */
+
   public GameObjectRecord getGameObjectRecord(int id) {
     List<GameObjectRecord> ret = toGameObjectRecords();
     for (GameObjectRecord record : ret) {
@@ -57,12 +113,22 @@ public class GameObjectContainer {
         return record;
       }
     }
+    LOGGER.warn("No GameObject found for id " + id);
     return null;
   }
+
+  /**
+   * Adds the current state of all GameObjects in the container to the history stack.
+   */
 
   public void addStaticStateGameObjects() {
     gameObjectHistory.push(toGameObjectRecords());
   }
+
+  /**
+   * Restores each GameObject to its previous static state, by retrieving it from the top of the
+   * static history stack.
+   */
 
   public void toLastStaticStateGameObjects() {
     for (GameObjectRecord record : gameObjectHistory.peek()) {
@@ -70,10 +136,10 @@ public class GameObjectContainer {
     }
   }
 
-  private void callSetFromRecord(GameObjectRecord record) {
-    getGameObject(record.id()).setFromRecord(record);
-  }
-
+  /**
+   * //TODO JavaDoc
+   * @author Konur Nordberg
+   */
   public Set<Pair> getCollisionPairs() {
     Set<Pair> collisionPairs = new HashSet<>();
     List<GameObjectRecord> records = toGameObjectRecords();
@@ -90,6 +156,11 @@ public class GameObjectContainer {
       }
     }
     return collisionPairs;
+  }
+
+  //calls the GameObject setFromRecord to restore an individual GameObject from its record
+  private void callSetFromRecord(GameObjectRecord record) {
+    getGameObject(record.id()).setFromRecord(record);
   }
 
 }
