7b171e852ca23c50f2b9ff51dba9db5911006192
Noah Loewy <noah.loewy@duke.edu>
2024-04-17 23:07:01 -0400

    Completed Round Condition Test

---
+++ src/test/java/oogasalad/model/gameengine/player/AllPlayersCompletedRoundConditionTest.java
@@ -0,0 +1,45 @@
+package oogasalad.model.gameengine.player;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Map;
+import oogasalad.model.gameengine.turn.TurnPolicy;
+import org.junit.jupiter.api.Test;
+import oogasalad.model.gameengine.GameEngine;
+import oogasalad.model.gameengine.condition.AllPlayersCompletedRoundCondition;
+import oogasalad.model.gameengine.player.Player;
+import oogasalad.model.gameengine.player.PlayerContainer;
+
+public class AllPlayersCompletedRoundConditionTest {
+
+  @Test
+  public void testEvaluate_AllPlayersCompletedRound() {
+    Player player1 = new Player(1);
+    Player player2 = new Player(2);
+    PlayerContainer playerContainer = new PlayerContainer(Map.of(1,player1,2,player2));
+    GameEngine gameEngine = mock(GameEngine.class);
+    when(gameEngine.getPlayerContainer()).thenReturn(playerContainer);
+    AllPlayersCompletedRoundCondition condition = new AllPlayersCompletedRoundCondition(List.of());
+    player1.completeRound();
+    player2.completeRound();
+    assertTrue(condition.evaluate(gameEngine));
+  }
+
+  @Test
+  public void testEvaluate_NotAllPlayersCompletedRound() {
+    Player player1 = new Player(1);
+    Player player2 = new Player(2);
+    PlayerContainer playerContainer = new PlayerContainer(Map.of(1,player1,2,player2));
+    GameEngine gameEngine = mock(GameEngine.class);
+    when(gameEngine.getPlayerContainer()).thenReturn(playerContainer);
+    AllPlayersCompletedRoundCondition condition = new AllPlayersCompletedRoundCondition(List.of());
+    assertFalse(condition.evaluate(gameEngine));
+
+  }
+}
