8f367bf4493771184e2a2b1db3c632d3e1f14c9a
Noah Loewy <noah.loewy@duke.edu>
2024-04-20 09:36:53 -0400

    Rank Documentation

--- src/main/java/oogasalad/model/gameengine/rank/IDComparator.java
+++ src/main/java/oogasalad/model/gameengine/rank/IDComparator.java
@@ -2,8 +2,19 @@ package oogasalad.model.gameengine.rank;
 
 import oogasalad.model.api.PlayerRecord;
 
-public class IDComparator extends PlayerRecordComparator{
+/**
+ * A comparator for sorting PlayerRecords based on their player IDs.
+ */
+public class IDComparator extends PlayerRecordComparator {
 
+  /**
+   * Compares two PlayerRecord objects based on their player IDs.
+   *
+   * @param o1 the first PlayerRecord to compare
+   * @param o2 the second PlayerRecord to compare
+   * @return zero. This is a no-op, as we will compare based on their IDs, as the tiebreaker
+   * specifies.
+   */
   @Override
   protected int compareMe(PlayerRecord o1, PlayerRecord o2) {
     return 0;
--- src/main/java/oogasalad/model/gameengine/rank/LowestScoreComparator.java
+++ src/main/java/oogasalad/model/gameengine/rank/LowestScoreComparator.java
@@ -2,11 +2,21 @@ package oogasalad.model.gameengine.rank;
 
 import oogasalad.model.api.PlayerRecord;
 
+/**
+ * A comparator for sorting PlayerRecords based on the lowest score.
+ */
 public class LowestScoreComparator extends PlayerRecordComparator {
 
+  /**
+   * Compares two PlayerRecord objects based on their scores, sorting them in ascending order.
+   *
+   * @param o1 the first PlayerRecord to compare
+   * @param o2 the second PlayerRecord to compare
+   * @return a negative integer, zero, or a positive integer as the first argument is less than,
+   * equal to, or greater than the second based on their scores.
+   */
   @Override
   public int compareMe(PlayerRecord o1, PlayerRecord o2) {
     return Double.compare(o1.score(), o2.score());
   }
-
 }
