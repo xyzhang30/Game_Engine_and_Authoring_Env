49b5c05f7969086538d990afe4bb87a79cc7d593
Jordan Haytaian <jeh120@duke.edu>
2024-04-20 23:57:41 -0400

    javadoc in sceneElementStyler

--- src/main/java/oogasalad/view/game_environment/scene_management/SceneElementStyler.java
+++ src/main/java/oogasalad/view/game_environment/scene_management/SceneElementStyler.java
@@ -4,16 +4,29 @@ import java.net.URL;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 
+/**
+ * Styles scene elements using css style sheets
+ *
+ * @author Jordan Haytaian
+ */
 public class SceneElementStyler {
-
   private final Pane root;
   private final String cssPath = "/view/styles.css";
 
+  /**
+   * Constructor adds style sheet to root
+   * @param root root node of scene
+   */
   public SceneElementStyler(Pane root) {
     this.root = root;
     addStyleSheetToRoot();
   }
 
+  /**
+   * Styles an element using the specified style tag
+   * @param node the element to be styled
+   * @param style the css style tag to add
+   */
   public void style(Node node, String style) {
     node.getStyleClass().add(style);
   }
