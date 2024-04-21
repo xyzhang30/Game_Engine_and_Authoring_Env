4da8cf33551746bfc8e4a8d52c2db45654a48eb6
Noah Loewy <noah.loewy@duke.edu>
2024-04-21 13:50:38 -0400

    Fixed Test

--- src/test/java/oogasalad/model/GameBuilderTest.java
+++ src/test/java/oogasalad/model/GameBuilderTest.java
@@ -46,7 +46,7 @@ public class GameBuilderTest {
 
     this.testBuilderDirector.constructCollidableObjects(List.of(co1, co2, co3));
 
-    ParserPlayer p1 = new ParserPlayer(1, List.of(2));
+    ParserPlayer p1 = new ParserPlayer(1, List.of(2), List.of());
 
     this.testBuilderDirector.constructPlayers(List.of(p1));
 
