c254e9d04b59f5ac056af9e6ce272a87aadfa5c7
Noah Loewy <noah.loewy@duke.edu>
2024-04-23 17:15:00 -0400

    Document Static Checkers

--- src/main/java/oogasalad/model/api/GameObjectRecord.java
+++ src/main/java/oogasalad/model/api/GameObjectRecord.java
@@ -1,5 +1,24 @@
 package oogasalad.model.api;
 
+/**
+ * Represents an object with properties such as mass, position, velocity, visibility, etc.
+ *
+ * @param id            The unique identifier of the object.
+ * @param mass          The mass of the object.
+ * @param x             The x-coordinate of the object's position.
+ * @param y             The y-coordinate of the object's position.
+ * @param velocityX     The velocity of the object along the x-axis.
+ * @param velocityY     The velocity of the object along the y-axis.
+ * @param visible       Indicates whether the object is visible or not.
+ * @param staticMu      The coefficient of static friction.
+ * @param kineticMu     The coefficient of kinetic friction.
+ * @param inclineAngle  The angle of inclination if the object is on an inclined plane.
+ * @param width         The width of the object.
+ * @param height        The height of the object.
+ * @param inelastic     Indicates whether collisions with this object are inelastic.
+ * @param phaser        True iff object phases through inelastic objects.
+ */
+
 public record GameObjectRecord(int id, double mass, double x, double y, double velocityX,
                                double velocityY, boolean visible, double staticMu, double kineticMu,
                                double inclineAngle, double width,
--- src/main/java/oogasalad/model/gameengine/checkstatic/VelocityStaticChecker.java
+++ src/main/java/oogasalad/model/gameengine/checkstatic/VelocityStaticChecker.java
@@ -5,15 +5,34 @@ import oogasalad.model.annotations.IsCommand;
 import oogasalad.model.annotations.VariableParamNumber;
 import oogasalad.model.api.GameObjectRecord;
 
+/**
+ * Represents a velocity-based static checker for game objects.
+ *
+ * @author Noah Loewy
+ */
+
 @IsCommand(isCommand = true)
 public class VelocityStaticChecker implements StaticChecker {
 
   private final List<Integer> arguments;
 
+  /**
+   * Constructs a VelocityStaticChecker with variable parameters.
+   *
+   * @param arguments The list of game objects that this velocity static checker applies to.
+   */
+
   @VariableParamNumber(isVariable = true)
   public VelocityStaticChecker(List<Integer> arguments) {
     this.arguments = arguments;
   }
+
+  /**
+   * Checks if the given game object record is static based on velocity.
+   *
+   * @param record The game object record to be checked.
+   * @return true if the object has negligible velocity or is not visible, false otherwise.
+   */
 
   @Override
   public boolean isStatic(GameObjectRecord record) {
