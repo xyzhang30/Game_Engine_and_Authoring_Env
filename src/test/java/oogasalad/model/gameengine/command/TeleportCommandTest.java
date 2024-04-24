6dda86d5a53fa457a3e9cb3108a9ca8ed04f13eb
Noah Loewy <noah.loewy@duke.edu>
2024-04-24 00:59:11 -0400

    All Cmds Tested

---
+++ src/test/java/oogasalad/model/gameengine/command/TeleportCommandTest.java
@@ -0,0 +1,31 @@
+package oogasalad.model.gameengine.command;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import oogasalad.model.gameengine.GameEngine;
+import oogasalad.model.gameengine.gameobject.GameObject;
+
+public class TeleportCommandTest {
+
+  @Test
+  public void testExecute() {
+    GameObject gameObjectFrom = mock(GameObject.class);
+    GameObject gameObjectTo = mock(GameObject.class);
+    GameEngine engine = mock(GameEngine.class);
+    Map<Integer, GameObject> gameObjectMap = Map.of(1, gameObjectFrom, 2, gameObjectTo);
+
+    TeleportCommand command = new TeleportCommand(Arrays.asList(1, 2), gameObjectMap);
+
+    command.execute(engine);
+
+    verify(gameObjectFrom, times(1)).teleportTo(gameObjectTo);
+  }
+}
+
+
+
+
