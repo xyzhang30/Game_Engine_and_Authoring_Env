9eef2875130a756515ac573535874ec7e779300c
Jordan Haytaian <jeh120@duke.edu>
2024-04-25 22:24:52 -0400

    refactooring resources folder and converting game management class strings to properties file values

--- src/main/java/oogasalad/view/scene_management/SceneElementStyler.java
+++ src/main/java/oogasalad/view/scene_management/SceneElementStyler.java
@@ -16,9 +16,9 @@ public class SceneElementStyler {
 
   private final Pane root;
   private Map<ThemeType, String> themePathMap;
-  private final String defaultPath = "/view/default_style.css";
-  private final String darkPath = "/view/dark_style.css";
-  private final String funPath = "/view/fun_style.css";
+  private final String defaultPath = "/view/styles/default_style.css";
+  private final String darkPath = "/view/styles/dark_style.css";
+  private final String funPath = "/view/styles/fun_style.css";
 
   /**
    * Constructor adds style sheet to root
