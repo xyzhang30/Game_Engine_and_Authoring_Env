e25ce2578a434bc36d6f9f2c4df86d43cc2a3148
Konur Nordberg <ken24@duke.edu>
2024-04-26 12:13:49 -0400

    made a cloud database through amazon aws and configured this class to connect to this server

--- src/main/java/oogasalad/model/database/DatabaseConfig.java
+++ src/main/java/oogasalad/model/database/DatabaseConfig.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 
 public class DatabaseConfig {
-  private static final String URL = "jdbc:postgresql://localhost:5432/fysicsfundatabase";
+  private static final String URL = "jdbc:postgresql://fysicsfun.c7m0ky02uhmn.us-east-2.rds.amazonaws.com:5432/fysicsfundatabase2";
   private static final String USER = "konurnordberg";
-  private static final String PASSWORD = "cs308";
+  private static final String PASSWORD = "wehavethebestprojectbyfar";
 
   public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
