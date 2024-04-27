33f1606566fd6a360e12d74863e0c21a6d5614cd
Jordan Haytaian <jeh120@duke.edu>
2024-04-26 22:23:23 -0400

    debugging print statements

---
+++ src/test/java/oogasalad/view/LanguageScreenTest.java
@@ -0,0 +1,9 @@
+package oogasalad.view;
+
+import javafx.stage.Window;
+import util.DukeApplicationTest;
+
+public class LanguageScreenTest extends DukeApplicationTest {
+  Window window = new Window();
+
+}
