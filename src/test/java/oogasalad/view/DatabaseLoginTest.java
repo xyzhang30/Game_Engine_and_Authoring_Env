63dd4ef6ab2ed33065c56dd7d19efafd1914071e
Konur Nordberg <ken24@duke.edu>
2024-04-28 09:07:34 -0400

    set up database login test

---
+++ src/test/java/oogasalad/view/DatabaseLoginTest.java
@@ -0,0 +1,29 @@
+package oogasalad.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import oogasalad.model.database.Database;
+import oogasalad.view.controller.DatabaseController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import oogasalad.view.database.Leaderboard;
+
+public class DatabaseControllerTest {
+
+  private DatabaseController databaseController;
+  private Database databaseView;
+  private List<String> currentPlayersManager;
+  private Leaderboard leaderboard;
+
+  @BeforeEach
+  public void setUp() {
+    databaseView = mock(Database.class);
+    databaseController = new DatabaseController(leaderboard, currentPlayersManager);
+  }
+
+
+
+}
