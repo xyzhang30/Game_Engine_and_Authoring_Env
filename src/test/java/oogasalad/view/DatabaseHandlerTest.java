5d8d81dd132d159bc30082994b84beddfd71ea83
Konur Nordberg <ken24@duke.edu>
2024-04-28 09:44:00 -0400

    setup for the handler test class

--- src/test/java/oogasalad/view/DatabaseHandlerTest.java
+++ src/test/java/oogasalad/view/DatabaseHandlerTest.java
@@ -1,5 +1,16 @@
 package oogasalad.view;
 
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import oogasalad.view.controller.DatabaseController;
+import oogasalad.view.controller.GameController;
+import oogasalad.view.scene_management.scene_managers.SceneManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.ArrayList;
+
 public class DatabaseHandlerTest {
 
 }
