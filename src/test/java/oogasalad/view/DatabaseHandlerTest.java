735e4931cfbb85a6c3e2d4d33cc3c07b3a4e8fd9
Konur Nordberg <ken24@duke.edu>
2024-04-28 09:44:43 -0400

    refining setup for dbhandler test

--- src/test/java/oogasalad/view/DatabaseHandlerTest.java
+++ src/test/java/oogasalad/view/DatabaseHandlerTest.java
@@ -4,13 +4,28 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import oogasalad.view.controller.DatabaseController;
 import oogasalad.view.controller.GameController;
+import oogasalad.view.scene_management.scene_element.scene_element_handler.DatabaseHandler;
 import oogasalad.view.scene_management.scene_managers.SceneManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
 import java.util.ArrayList;
+import static org.mockito.Mockito.*;
 
 public class DatabaseHandlerTest {
+
+  @Mock
+  private GameController gameController;
+  @Mock
+  private SceneManager sceneManager;
+  @Mock
+  private DatabaseController databaseController;
+  @Mock
+  private Pane mockNode;
+
+  private DatabaseHandler databaseHandler;
+  private TextField usernameTextField;
+  private TextField passwordField;
 
 }
