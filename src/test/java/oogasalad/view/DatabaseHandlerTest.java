060edfafa1c830e730f360aced493a6fb2945cef
Konur Nordberg <ken24@duke.edu>
2024-04-28 10:17:13 -0400

    added a test to ensure score is properly gotten from Id

--- src/test/java/oogasalad/view/DatabaseHandlerTest.java
+++ src/test/java/oogasalad/view/DatabaseHandlerTest.java
@@ -27,6 +27,33 @@ public class DatabaseHandlerTest {
     databaseController = new DatabaseController(leaderboard, currentPlayersManager);
   }
 
+  @Test
+  public void testGetScoreFromId() {
+    // Setup a list of PlayerRecord mocks
+    PlayerRecord player1 = mock(PlayerRecord.class);
+    PlayerRecord player2 = mock(PlayerRecord.class);
+
+    // Configure mocks
+    when(player1.playerId()).thenReturn(1);
+    when(player1.score()).thenReturn(100.0);
+    when(player2.playerId()).thenReturn(2);
+    when(player2.score()).thenReturn(200.0);
+
+    List<PlayerRecord> players = List.of(player1, player2);
+
+    // Test finding an existing ID
+    int score1 = databaseController.getScoreFromId(players, 1);
+    assertEquals(100, score1, "Score should match for player ID 1");
+
+    // Test finding another existing ID
+    int score2 = databaseController.getScoreFromId(players, 2);
+    assertEquals(200, score2, "Score should match for player ID 2");
+
+    // Test with an ID that does not exist
+    int score3 = databaseController.getScoreFromId(players, 3);
+    assertEquals(0, score3, "Score should be 0 for a non-existent player ID");
+  }
+
 
 
 
