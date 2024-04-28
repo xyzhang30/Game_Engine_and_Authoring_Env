744cb22a862d3748ad9760e9eb73ebf767e18a34
Konur Nordberg <ken24@duke.edu>
2024-04-28 09:53:11 -0400

    setup

--- src/test/java/oogasalad/view/LeaderBoardTest.java
+++ src/test/java/oogasalad/view/LeaderBoardTest.java
@@ -1,5 +1,17 @@
 package oogasalad.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import oogasalad.view.controller.DatabaseController;
+import oogasalad.view.scene_management.scene_element.scene_element_handler.DatabaseHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
 public class LeaderBoardTest {
 
 }
