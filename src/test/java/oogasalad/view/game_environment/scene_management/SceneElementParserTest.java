a076d76d3c947ca39b270f558417866dca891fc8
Doga Ozmen <odo4@duke.edu>
2024-04-29 15:15:54 -0400

    scene element parses has been tested

--- src/test/java/oogasalad/view/game_environment/scene_management/SceneElementParserTest.java
+++ src/test/java/oogasalad/view/game_environment/scene_management/SceneElementParserTest.java
@@ -1,5 +1,53 @@
 package oogasalad.view.game_environment.scene_management;
 
-public class SceneElementParserTest {
+import oogasalad.view.scene_management.element_parsers.SceneElementParser;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the SceneElementParser class.
+ *
+ * @author Doga Ozmen
+ */
+public class SceneElementParserTest extends ApplicationTest {
+
+  private Path tempXmlFile;
+  private SceneElementParser parser;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    tempXmlFile = Files.createTempFile("test", ".xml");
+    parser = new SceneElementParser();
+
+    String xmlContent = "<root><node><name>TestElement</name><type>Button</type></node></root>";
+    Files.writeString(tempXmlFile, xmlContent);
+  }
+
+  @AfterEach
+  public void tearDown() throws IOException {
+    Files.deleteIfExists(tempXmlFile);
+  }
+
+  /**
+   * Tests getting element parameters from a file.
+   */
+  @Test
+  public void testGetElementParametersFromFile() throws Exception {
+    List<Map<String, String>> parameters = parser.getElementParametersFromFile(tempXmlFile.toString());
+
+    assertEquals(1, parameters.size(), "Should have one element map");
+    Map<String, String> paramMap = parameters.get(0);
+    assertEquals("TestElement", paramMap.get("name"), "Name should match");
+    assertEquals("Button", paramMap.get("type"), "Type should match");
+  }
 }
