549969b4a4bcfb03ad5d2d5d66600a3e4a6a0069
Noah Loewy <noah.loewy@duke.edu>
2024-04-28 20:52:08 -0400

    Reorganized Class

--- src/main/java/oogasalad/view/scene_management/scene_element/scene_element_handler/GamePlayManagementHandler.java
+++ src/main/java/oogasalad/view/scene_management/scene_element/scene_element_handler/GamePlayManagementHandler.java
@@ -29,17 +29,6 @@ public class GamePlayManagementHandler extends Handler {
     super(gameController, sceneManager);
   }
 
-  /**
-   * Creates an event handler for the specified node and event type. The handler will invoke the
-   * appropriate event function when the event occurs on the given node.
-   *
-   * @param node  The node to which the event handler will be attached.
-   * @param event The event type as a string that specifies the event to handle.
-   */
-  public void createElementHandler(Node node, String event) {
-    Consumer<Node> consumer = eventMap.get(SceneElementEvent.valueOf(event));
-    consumer.accept(node);
-  }
 
   protected void createEventMap() {
     eventMap = new HashMap<>();
@@ -80,6 +69,18 @@ public class GamePlayManagementHandler extends Handler {
       getGameController().changeMod(selectedMod);
       getSceneManager().getRoot().requestFocus();
     });
+  }
+
+  /**
+   * Creates an event handler for the specified node and event type. The handler will invoke the
+   * appropriate event function when the event occurs on the given node.
+   *
+   * @param node  The node to which the event handler will be attached.
+   * @param event The event type as a string that specifies the event to handle.
+   */
+  public void createElementHandler(Node node, String event) {
+    Consumer<Node> consumer = eventMap.get(SceneElementEvent.valueOf(event));
+    consumer.accept(node);
   }
 
 }
